243f27c9bdc2a1490e69874c9d55ab3e498693df
Ольга Чернецкая <chernetzkayaolga@yandex.by>
2023-08-27 13:46:09 +0300

    сприн 8 тема 4 test_routes сделано

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -41,7 +41,7 @@ class TestRoutes(TestCase):
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_availability_for_note_edit_and_delete_and_add(self):
+    def test_availability_for_note_edit_and_delete_and_detail(self):
         users_statuses = (
             (self.author, HTTPStatus.OK),
             (self.reader, HTTPStatus.NOT_FOUND),
@@ -54,12 +54,26 @@ class TestRoutes(TestCase):
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
 
+    def test_availability_for_authenticated_user(self):
+        self.client.force_login(self.reader)
+        for name in ('notes:list', 'notes:success', 'notes:add'):
+            with self.subTest(user=self.reader, name=name):
+                url = reverse(name)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
     def test_redirect_for_anonymous_client(self):
         login_url = reverse('users:login')
-        for name in ('notes:edit', 'notes:delete', 'notes:detail'):
+        for name in ('notes:detail', 'notes:edit', 'notes:delete'):
             with self.subTest(name=name):
                 url = reverse(name, args=(self.notes.slug,))
                 redirect_url = f'{login_url}?next={url}'
                 response = self.client.get(url)
                 self.assertRedirects(response, redirect_url)
 
+        for name in ('notes:list', 'notes:success', 'notes:add'):
+            with self.subTest(name=name):
+                url = reverse(name)
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
