89c4116044cbd3129c6cb4070dba31fec4335b67
Ольга Чернецкая <chernetzkayaolga@yandex.by>
2023-08-28 14:03:00 +0300

    сприн 8 тема 4 test_logic п1 и п2 сделано

---
+++ tests/test_logic.py
@@ -0,0 +1,47 @@
+from http import HTTPStatus
+
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from notes.forms import WARNING
+from notes.models import Note
+
+User = get_user_model()
+
+
+class TestNoteCreation(TestCase):
+
+    TITLE = 'Заголовок'
+    TEXT = 'Текст'
+    SLUG = 'slugs'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(username='Мимо Крокодил')
+        cls.auth_client = Client()
+        cls.auth_client.force_login(cls.user)
+        cls.form_data = {
+            'title': cls.TITLE,
+            'text': cls.TEXT,
+            'slug': cls.SLUG,
+            'author': cls.user
+        }
+        cls.url = reverse('notes:add')
+
+    def test_anonymous_user_cant_create_note(self):
+        self.client.post(self.url, data=self.form_data)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 0)
+
+    def test_user_can_create_comment(self):
+        response = self.auth_client.post(self.url, data=self.form_data)
+        done_url = reverse('notes:success')
+        self.assertRedirects(response, f'{done_url}')
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
+        note = Note.objects.get()
+        self.assertEqual(note.title, self.TITLE)
+        self.assertEqual(note.text, self.TEXT)
+        self.assertEqual(note.slug, self.SLUG)
+        self.assertEqual(note.author, self.user)
